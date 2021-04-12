47ce73c4e0bbaf3479aad1e311da65f728a97604
Randall John <randall.john36@gmail.com>
2021-04-11 20:15:26 -0400

    styled header component

--- marketview/src/App.js
+++ marketview/src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import './App.css'
+import Header from './components/Header.js'
+import React, { useState } from 'react'
+
+
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //func we will eventually use to log the user out, and pass it into header as a prop
+  const logOut = () => {
+    console.log("User logged out")
+    if (isLoggedIn === true) {
+      setIsLoggedIn(false)
+    }
+  }
+
   return (
     <div>
-      
+      <Header title = "Home" username = "User23" logOut = {logOut}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- marketview/src/components/Header.js
+++ marketview/src/components/Header.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 
-function Header({ title, username, isLoggedIn, logIn, logOut }) {
-
-    let logInOrOut = isLoggedIn ? "Log Out" : "Log In"
+function Header({ title, username, logOut }) {
 
     return (
         <div className = {"headerContainer"}>
             <h1 className = {"headerTitle"}>{title}</h1>
-            <button className = {"log"} onClick = {isLoggedIn ? logOut : logIn}>{logInOrOut}</button>
-            <h4 className ={"loggedInMessage"}>Logged In: {username}</h4>
+            <button className = {"logOutButton"} onClick = {logOut}>Log Out</button>
+            <h5 className ={"loggedInMessage"}>Logged In: {username}</h5>
         </div>
     )
 }
