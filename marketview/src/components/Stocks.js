3a06a995f6f3eca2d28a2f7636ebadb06da43aec
Randall John <randall.john36@gmail.com>
2021-04-19 18:05:54 -0400

    added search bar to stocks

--- marketview/src/components/Stocks.js
+++ marketview/src/components/Stocks.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Navbar from './Navbar'
 
 
 function Stocks({ logOut, username }) {
+
+    const [searchTerm, setSearchTerm] = useState("");
+
     return (
         <div>
             <Header title = "Stocks" username = {username} logOut = {logOut} />
             <Navbar />
             <body>
-                <h1>Stocks Page</h1>
+                <div className = {"stocksSearch"}>
+                    <input
+                        type = "text"
+                        placeholder = "Enter a Stock Ticker"
+                        onChange = {(event) => {
+                            setSearchTerm(event.target.value)
+                        }}
+                    />
+                    <i class="fa fa-search" aria-hidden="true"/>
+                </div>
             </body>
         </div>
     )
