ae79ddba788d622f7bf0fcf5f9627218638c542b
Randall John <randall.john36@gmail.com>
2021-04-19 18:45:39 -0400

    styled the search bar in stocks

--- marketview/src/components/Stocks.js
+++ marketview/src/components/Stocks.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Header from './Header'
 import Navbar from './Navbar'
+import '../css/Stocks.css';
 
 
 function Stocks({ logOut, username }) {
@@ -12,15 +13,17 @@ function Stocks({ logOut, username }) {
             <Header title = "Stocks" username = {username} logOut = {logOut} />
             <Navbar />
             <body>
-                <div className = {"stocksSearch"}>
-                    <input
-                        type = "text"
-                        placeholder = "Enter a Stock Ticker"
-                        onChange = {(event) => {
+                <div className = {"searchContainer"}>
+                    <div className = {"inputBox"}>
+                      <input
+                         type = "text"
+                         placeholder = "Enter a Stock Ticker"
+                         onChange = {(event) => {
                             setSearchTerm(event.target.value)
-                        }}
-                    />
-                    <i class="fa fa-search" aria-hidden="true"/>
+                         }}
+                     />
+                        <i class="fa fa-search" aria-hidden="true"/>
+                    </div>
                 </div>
             </body>
         </div>
