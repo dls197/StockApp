da10aad65feea02575c699512c12627f5af537c1
Randall John <randall.john36@gmail.com>
2021-04-24 18:24:35 -0400

    changed src folder name back

---
+++ marketview/src/components/LoginModal.js
@@ -0,0 +1,31 @@
+import react from 'react'
+import Modal from 'react-modal'
+import '../css/LoginModal.css'
+
+
+Modal.setAppElement('#root')
+
+function LoginModal({ showLoginModal }) {
+
+    const customStyles = {
+        overlay : {
+            backgroundColor: 'grey'
+        },
+        content : {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)'
+        }
+    }
+    
+    return (
+        <div className = {"loginContainer"}>
+            <div className = {"signup"}>
+                
+            </div>
+        </div>
+    )
+}
