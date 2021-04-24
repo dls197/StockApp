e7a6c90ecaab0d19b826825fdf55b14903d5273d
Randall John <randall.john36@gmail.com>
2021-04-24 17:03:52 -0400

    connected database to proj

--- marketview/server/index.js
+++ marketview/server/index.js
@@ -4,7 +4,19 @@ express, body-parser, mysql, nodemon, cors
 */
 
 const express = require('express')
+const mysql = require('mysql')
+
+
 const app = express()
+
+app.use(express.json())
+
+const database = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "Exexxexex2*",
+    database: "marketview"
+})
 
 app.listen(3001, () => {
     console.log("running on port 3001")
