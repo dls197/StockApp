0ba9b46015bd7fe6f030ab6468218a5b59517483
Randall John <randall.john36@gmail.com>
2021-04-28 13:24:37 -0400

    moved some props around
    in order to make stock fetching more flexible (so it can be used in myprofile and social without rewriting the code lol)

--- marketview/src/App.js
+++ marketview/src/App.js
@@ -15,6 +15,11 @@ function App() {
   const [password, setPassword] = useState("");
   const [newsData, setNewsData] = useState([]); //variable to store newsData
   const [showWelcomeModal, setShowWelcomeModal] = useState(true)
+  const [searchTerm, setSearchTerm] = useState("") //the ticker symbol they search for
+  const [xValues, setXValues] = useState([]) //dates fetched from api
+  const [yValues, setYValues] = useState([]) //prices fetched from api
+  const [showStockModal, setShowStockModal] = useState(false) //decide whether to show the stock graph modal or not
+
 
   //API Call for sending news article info
   const getNewsData = async () => {
@@ -23,6 +28,27 @@ function App() {
             .then(data => setNewsData(data));
   }
   
+  //API Call for fetching stocks info
+
+  function fetchStock(ticker) {
+    const apiKey = "BFW1POG1RFAENYS8"
+    let apiCall = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&outputsize=compact&apikey=${apiKey}`
+    let futureXVals = []
+    let futureYVals = []
+
+    fetch(apiCall)
+        .then(response => response.json())
+        .then(response => {
+            for (const item in response['Time Series (Daily)']) {
+                futureXVals.push(item);
+                futureYVals.push(response['Time Series (Daily)'][item]['1. open']);
+            }
+
+            setXValues(futureXVals)
+            setYValues(futureYVals)
+        })
+    }
+
   useEffect(() => {
     getNewsData();
   }, []);
@@ -65,7 +91,14 @@ function App() {
                                   setUsername = {setUsername}
                                   setPassword = {setPassword}
                                   setShowWelcomeModal = {setShowWelcomeModal}
-                                  setLoginStatus = {setLoginStatus} 
+                                  setLoginStatus = {setLoginStatus}
+                                  searchTerm = {searchTerm}
+                                  setSearchTerm = {setSearchTerm}
+                                  xValues = {xValues}
+                                  yValues = {yValues}
+                                  showStockModal = {showStockModal}
+                                  setShowStockModal = {setShowStockModal}
+                                  fetchStock = {fetchStock} 
                                 />}
           />
           <Route 
--- marketview/src/components/Stocks.js
+++ marketview/src/components/Stocks.js
@@ -6,42 +6,17 @@ import './StockModal'
 import StockModal from './StockModal';
 
 
-function Stocks({ username, setUsername, setPassword, setShowWelcomeModal, setLoginStatus }) {
+function Stocks({ username, setUsername, setPassword, 
+                setShowWelcomeModal, setLoginStatus, 
+                searchTerm, setSearchTerm, xValues,
+                yValues, showStockModal, setShowStockModal,
+                fetchStock }) {
 
-    //the ticker symbol they search for
-    const [searchTerm, setSearchTerm] = useState("")
-    //dates fetched from api
-    const [xValues, setXValues] = useState([])
-    //prices fetched from api
-    const [yValues, setYValues] = useState([])
-    //decide whether to show the stock graph modal or not
-    const [showStockModal, setShowStockModal] = useState(false)
-
-
-    function fetchStock() {
-        const apiKey = "BFW1POG1RFAENYS8"
-        let tickerSymbol = searchTerm
-        let apiCall = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${tickerSymbol}&outputsize=compact&apikey=${apiKey}`
-        let futureXVals = [];
-        let futureYVals = [];
     
-        fetch(apiCall)
-            .then(response => response.json())
-            .then(response => {
-                for (const item in response['Time Series (Daily)']) {
-                    futureXVals.push(item);
-                    futureYVals.push(response['Time Series (Daily)'][item]['1. open']);
-                }
-    
-                setXValues(futureXVals)
-                setYValues(futureYVals)
-            })
-        }
-
     //fetch the stock data whenever the form gets submitted
     function handleSubmit(event) {
         event.preventDefault()
-        fetchStock()
+        fetchStock(searchTerm)
         setShowStockModal(true)
     }
 
