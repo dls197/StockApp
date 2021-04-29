9357076fa083303d7b27b739653717a451d7815d
ZemyCode <69816894+ZemyCode@users.noreply.github.com>
2021-04-29 18:33:24 -0400

    Updated News Page slightly, more proper flex boxes now

--- marketview/src/components/News.js
+++ marketview/src/components/News.js
@@ -32,108 +32,96 @@ function News({ username, newsData, setUsername, setPassword, setShowWelcomeModa
             />
             <Navbar />
             <div className="articles">
-                    <section className="articleContent">
-                        <div className="articleContent-body">
-                            <div className="articleCard">
-                                <div className="cardText">
-                                    <h1>{article1.headline}</h1>
-                                    <p>{article1.summary}</p>
-                                    <div className = "cardSource">
-                                        <p>{article1.source}</p>
-                                        <p className="cardTime">{article1Time}</p>
-                                    </div>
-                                </div>
-                                <div className="cardImage">
-                                    <a href={article1.url}> 
-                                        <img src={article1.image} alt="Article 1 Photograph" />
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="articleCard">
-                                <div className="cardText">
-                                    <h1>{article2.headline}</h1>
-                                    <p>{article2.summary}</p>
-                                    <div className = "cardSource">
-                                        <p>{article2.source}</p>
-                                        <p className="cardTime">{article2Time}</p>
-                                    </div>
-                                </div>
-                                <div className="cardImage">
-                                    <a href={article2.url}> 
-                                        <img src={article2.image} alt="Article 2 Photograph" />
-                                    </a>
-                                </div>
-                            </div>
+                <div className="articleCard">
+                    <div className="cardText">
+                        <h1>{article1.headline}</h1>
+                        <p>{article1.summary}</p>
+                        <div className = "cardSource">
+                            <p>{article1.source}</p>
+                            <p className="cardTime">{article1Time}</p>
                         </div>
-                    </section>
-                    <section className="articleContent">
-                        <div className="articleContent-body">
-                            <div className="articleCard">
-                                <div className="cardText">
-                                    <h1>{article3.headline}</h1>
-                                    <p>{article3.summary}</p>
-                                    <div className = "cardSource">
-                                        <p>{article3.source}</p>
-                                        <p className="cardTime">{article3Time}</p>
-                                    </div>
-                                </div>
-                                <div className="cardImage">
-                                    <a href={article3.url}> 
-                                        <img src={article3.image} alt="Article 3 Photograph" />
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="articleCard">
-                                <div className="cardText">
-                                    <h1>{article4.headline}</h1>
-                                    <p>{article4.summary}</p>
-                                    <div className = "cardSource">
-                                        <p>{article4.source}</p>
-                                        <p className="cardTime">{article4Time}</p>
-                                    </div>
-                                </div>
-                                <div className="cardImage">
-                                    <a href={article4.url}> 
-                                        <img src={article4.image} alt="Article 4 Photograph" />
-                                    </a>
-                                </div>
-                            </div>
+                    </div>
+                    <div className="cardImage">
+                        <a href={article1.url}> 
+                            <img src={article1.image} alt="Article 1 Photograph" />
+                        </a>
+                    </div>
+                </div>
+                <div className="articleCard">
+                    <div className="cardText">
+                        <h1>{article2.headline}</h1>
+                        <p>{article2.summary}</p>
+                        <div className = "cardSource">
+                            <p>{article2.source}</p>
+                            <p className="cardTime">{article2Time}</p>
                         </div>
-                    </section>
-                    <section className="articleContent">
-                        <div className="articleContent-body">
-                            <div className="articleCard">
-                                <div className="cardText">
-                                    <h1>{article5.headline}</h1>
-                                    <p>{article5.summary}</p>
-                                    <div className = "cardSource">
-                                        <p>{article5.source}</p>
-                                        <p className="cardTime">{article5Time}</p>
-                                    </div>
-                                </div>
-                                <div className="cardImage">
-                                    <a href={article5.url}> 
-                                        <img src={article5.image} alt="Article 5 Photograph" />
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="articleCard">
-                                <div className="cardText">
-                                    <h1>{article6.headline}</h1>
-                                    <p>{article6.summary}</p>
-                                    <div className = "cardSource">
-                                        <p>{article6.source}</p>
-                                        <p className="cardTime">{article6Time}</p>
-                                    </div>
-                                </div>
-                                <div className="cardImage">
-                                    <a href={article6.url}> 
-                                        <img src={article6.image} alt="Article 6 Photograph" />
-                                    </a>
-                                </div>
-                            </div>
+                    </div>
+                    <div className="cardImage">
+                        <a href={article2.url}> 
+                            <img src={article2.image} alt="Article 2 Photograph" />
+                        </a>
+                    </div>
+                </div>
+                <div className="articleCard">
+                    <div className="cardText">
+                        <h1>{article3.headline}</h1>
+                        <p>{article3.summary}</p>
+                        <div className = "cardSource">
+                            <p>{article3.source}</p>
+                            <p className="cardTime">{article3Time}</p>
                         </div>
-                    </section>
+                    </div>
+                    <div className="cardImage">
+                        <a href={article3.url}> 
+                            <img src={article3.image} alt="Article 3 Photograph" />
+                        </a>
+                    </div>
+                </div>
+                <div className="articleCard">
+                    <div className="cardText">
+                        <h1>{article4.headline}</h1>
+                        <p>{article4.summary}</p>
+                        <div className = "cardSource">
+                            <p>{article4.source}</p>
+                            <p className="cardTime">{article4Time}</p>
+                        </div>
+                    </div>
+                    <div className="cardImage">
+                        <a href={article4.url}> 
+                            <img src={article4.image} alt="Article 4 Photograph" />
+                        </a>
+                    </div>
+                </div>
+                <div className="articleCard">
+                    <div className="cardText">
+                        <h1>{article5.headline}</h1>
+                        <p>{article5.summary}</p>
+                        <div className = "cardSource">
+                            <p>{article5.source}</p>
+                            <p className="cardTime">{article5Time}</p>
+                        </div>
+                    </div>
+                    <div className="cardImage">
+                        <a href={article5.url}> 
+                            <img src={article5.image} alt="Article 5 Photograph" />
+                        </a>
+                    </div>
+                </div>
+                <div className="articleCard">
+                    <div className="cardText">
+                        <h1>{article6.headline}</h1>
+                        <p>{article6.summary}</p>
+                        <div className = "cardSource">
+                            <p>{article6.source}</p>
+                            <p className="cardTime">{article6Time}</p>
+                        </div>
+                    </div>
+                    <div className="cardImage">
+                        <a href={article6.url}> 
+                            <img src={article6.image} alt="Article 6 Photograph" />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
