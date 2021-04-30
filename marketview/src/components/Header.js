15add25244e58a9f8f377c5a607118066db759f8
ZemyCode <69816894+ZemyCode@users.noreply.github.com>
2021-04-30 15:26:39 -0400

    logo added to header, also white text so more visible

--- marketview/src/components/Header.js
+++ marketview/src/components/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from './../images/marketviewlogo.png'
 
 function Header({ title, username, setUsername, setPassword, setShowWelcomeModal, setLoginStatus}) {
 
     const navStyle = {
         color: 'black'
+    }
+
+    const headerTextStyle = {
+        color: `aliceblue`
+    }
+
+    const logoStyle = {
+        margin: `0`,
+        position: `absolute`,
+        top: `25%`
     }
 
     const logOut = () => {
@@ -16,11 +27,18 @@ function Header({ title, username, setUsername, setPassword, setShowWelcomeModal
 
     return (
         <div className = {"headerContainer"}>
-            <h1 className = {"headerTitle"}>{title}</h1>
+            <Link style= {logoStyle} to = "/">
+                <img src={logo} alt="MarketViewLogo"/>
+            </Link>
+            <Link style = {headerTextStyle} to = "/">
+                <h1 className = {"headerTitle"}>{title}</h1>
+            </Link>
             <Link style = {navStyle} to = "/">
             <button className = {"logOutButton"} onClick = {() => logOut()}>Log Out</button>
             </Link>
+            <Link style = {headerTextStyle} to = "/">
             <h5 className ={"loggedInMessage"}>Logged In: {username}</h5>
+            </Link>
         </div>
     )
 }
